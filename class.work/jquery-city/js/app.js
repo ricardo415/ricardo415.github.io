be740225c2251e5023bf42b0fdd7506c96108fe0
ricardo415 <34083049+ricardo415@users.noreply.github.com>
2018-01-09 20:43:18 -0800

    This code

--- class.work/jquery-city/js/app.js
+++ class.work/jquery-city/js/app.js
@@ -16,3 +16,17 @@ function changeToThird() {
 
 	$('#bigimage').attr('src', 'img/3.jpg')
 }
+
+$('#fourth').click(changeToFourth)
+function changeToFourth() {
+
+	$(this).img ('src', 'img/4.jpg')
+}
+
+$('.thumb').click(changeBigImage)
+function changeBigImage() {
+var imageUrl = $(this).attr('src');
+$('#bigimage').attr('src', imageUrl); 
+
+}
+
