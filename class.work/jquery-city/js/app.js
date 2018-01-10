52c28c4cda85dd064342b28d7b9e203b9328a55f
ricardo415 <34083049+ricardo415@users.noreply.github.com>
2018-01-09 20:18:00 -0800

    picture change

--- class.work/jquery-city/js/app.js
+++ class.work/jquery-city/js/app.js
@@ -1,2 +1,18 @@
-// 1. Create .click() handlers for each of the thumbnails: #first, #second, #third, #fourth
-// 2. Use .attr() to change the `src` attribute of #bigimage to correspond to image that was clicked
+$('#first').click(changeToFirst);
+function changeToFirst() {
+
+$('#bigimage').attr('src', 'img/1.jpg');
+
+}
+
+$('#second').click(changeToSecond);
+function changeToSecond() {
+	
+	$('#bigimage').attr('src', 'img/2.jpg')
+}
+
+$('#third').click(changeToThird);
+function changeToThird() {
+
+	$('#bigimage').attr('src', 'img/3.jpg')
+}
