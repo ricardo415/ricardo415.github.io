ec6320170b05d418e7c33c2f39e6768d18473821
ricardo415 <34083049+ricardo415@users.noreply.github.com>
2018-01-11 17:25:20 -0800

    -.-

--- homework/accordion/js/app.js
+++ homework/accordion/js/app.js
@@ -4,9 +4,7 @@
 // 2. Associate this .click() handler with an anoymous function (in other words, "pass" .click() and anonymous function)
 // 3. Inside the body of the anonymous function, use the concept of $(this) to .find() the <div class="wrapper">
 // 4. Once you find the .wrapper, use the .slideToggle() function to trigger the desired behavior
-$('.wrapper').slideToggle(dropDown)
+$('.row').click(dropDown);  
+function dropDown() 
 
-function dropDown() {
-
-var p = $(this).attr('p');
-$('#row').attr('p', 'row'); 
+ 
